157a75a67492d6e191e639223187561af08fb8d0
Geirr Winnem <gwinnem@gmail.com>
2018-08-09 11:25:01 +0200

    abElastic fix

--- js/app/common/directives/abElastic.js
+++ js/app/common/directives/abElastic.js
@@ -3,14 +3,13 @@
  * @version 1.0.0
  * @summary Auto resizing textarea dynamically..
  */
-
 (function () {
     "use strict";
-    angular.module("ab.common.textarea.autogrow", [])
+    angular.module("abTextAreaAutoresize", [])
         .directive("abElastic", function ($timeout) {
             return {
                 restrict: "A",
-                link: function ($scope, element, attrs, ctrl) {
+                link: function ($scope, element) {
                     element.css({ "height": "auto", "overflow-y": "hidden" });
                     $timeout(function () {
                         element.css("height", element[0].scrollHeight + "px");
@@ -22,44 +21,7 @@
 
                     });
                 }
-            }
+            };
         });
 
 }());
-// (function () {
-//     "use strict";
-//     angular.module("abTextAreaAutoresize", [])
-//         .directive("abElastic", function ($timeout) {
-//             return {
-//                 restrict: "A",
-//                 link: function (element) {
-//                     element.css({
-//                         //"height": "auto;",
-//                         "overflow-y": "hidden;"
-//                     });
-//                     var ta = element;
-//                     $timeout(function () {
-//                         // force text reflow
-                        
-//                         var text = ta.value;
-//                         ta.value = '';
-//                         ta.value = text;
-//                         var height = ta.scrollHeight;
-//                         if (height !== 0) {
-//                             $(ta).css("height", height + "px;");
-//                         }
-//                     }, 200);
-
-//                     element.on("input", function () {
-//                         element.css({
-//                             'height': "auto",
-//                             'overflow-y': "hidden"
-//                         });
-//                         var height = ta.scrollHeight;
-//                         $(ta).css("height", height + "px !important;");
-//                     });
-//                 }
-//             };
-//         });
-
-// }());
