6abf32cb17f74ddf5107d4addb9dd56719511a12
Geirr Winnem <gwinnem@gmail.com>
2018-09-03 13:07:01 +0200

    NA

--- js/app/common/directives/abWhenScrollEnds.js
+++ js/app/common/directives/abWhenScrollEnds.js
@@ -13,10 +13,29 @@
             return {
                 restrict: "A",
                 link: function (scope, element, attrs) {
+
+                    function debounce(func, wait, immediate) {
+                        var timeout;
+                        return function () {
+                            var context = this, args = arguments;
+                            var later = function () {
+                                timeout = null;
+                                if (!immediate) func.apply(context, args);
+                            };
+                            var callNow = immediate && !timeout;
+                            clearTimeout(timeout);
+                            timeout = setTimeout(later, wait);
+                            if (callNow) func.apply(context, args);
+                        };
+                    };
+                    function loadMore() {
+                        scope.$apply(attrs.whenScrollEnds);
+                    }
                     element.bind("scroll", function () {
                         var hiddenContentHeight = element.prop("scrollHeight") - element.height();
                         if (hiddenContentHeight - element.scrollTop() <= element.height()) {
-                            scope.$apply(attrs.whenScrollEnds);
+                            debounce(loadMore(), 2000);
+                            //scope.$apply(attrs.whenScrollEnds);
                         }
                     });
                 }
