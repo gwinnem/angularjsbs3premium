258222b5e13fdb0bc85b3e208fe41bfcc545cc58
Geirr Winnem <gwinnem@gmail.com>
2018-09-03 08:51:55 +0200

    Agni but nice example

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,46 @@ module.exports = function (grunt) { // jshint ignore:line
     // Browsersync
     //https://browsersync.io/docs/grunt
     grunt.loadNpmTasks('grunt-browser-sync');
-    
+
     // The default task (running 'grunt' in console) is 'watch'
     grunt.registerTask('default', ['connect', 'watch']);
 };
+
+// This shows a full config file!
+// module.exports = function (grunt) {
+//     grunt.initConfig({
+//         watch: {
+//             files: 'app/scss/**/*.scss',
+//             tasks: ['sass']
+//         },
+//         sass: {
+//             dev: {
+//                 files: {
+//                     'app/css/main.css': 'app/scss/main.scss'
+//                 }
+//             }
+//         },
+//         browserSync: {
+//             dev: {
+//                 bsFiles: {
+//                     src : [
+//                         'app/css/*.css',
+//                         'app/*.html'
+//                     ]
+//                 },
+//                 options: {
+//                     watchTask: true,
+//                     server: './app'
+//                 }
+//             }
+//         }
+//     });
+
+//     // load npm tasks
+//     grunt.loadNpmTasks('grunt-contrib-sass');
+//     grunt.loadNpmTasks('grunt-contrib-watch');
+//     grunt.loadNpmTasks('grunt-browser-sync');
+
+//     // define default task
+//     grunt.registerTask('default', ['browserSync', 'watch']);
+// };
