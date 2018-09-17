377e23f70135ebfcb633c1cd5c8cec6c85047b64
Geirr Winnem <gwinnem@gmail.com>
2018-09-17 08:08:01 +0200

    Text changes

--- js/app/common/controllers/abToggleSwitchCtrl.js
+++ js/app/common/controllers/abToggleSwitchCtrl.js
@@ -1,27 +1,34 @@
 ﻿/**
- * Directive for toggle switch input
+ * Controller for toggle switch input
  * @author Geirr Winnem
  * @version 1.0.0
  */
-(function () {
-    "use strict";
-    angular.module("abToggleSwitch", [[]])
-        .controller("ToggleSwitch", ["$scope", "$notification", "config",
-            function ($scope, $notification, config) {
-                $scope.enabled = true;
-                $scope.onOff = true;
-                $scope.yesNo = true;
-                $scope.disabled = true;
-                $scope.switchStatus = true;
-                $scope.switchStatus1 = true;
-                $scope.switchstatus2 = true;
-                $scope.switchStatus3 = true;
-                $scope.switchStatus4 = true;
-                $scope.switchStatus5 = true;
-                $scope.htmlSwitchStatus = true;
+(function() {
+  "use strict";
+  angular.module("abToggleSwitch", [[]]).controller("ToggleSwitch", [
+    "$scope",
+    "$notification",
+    "config",
+    function($scope, $notification, config) {
+      $scope.enabled = true;
+      $scope.onOff = true;
+      $scope.yesNo = true;
+      $scope.disabled = true;
+      $scope.switchStatus = true;
+      $scope.switchStatus1 = true;
+      $scope.switchstatus2 = true;
+      $scope.switchStatus3 = true;
+      $scope.switchStatus4 = true;
+      $scope.switchStatus5 = true;
+      $scope.htmlSwitchStatus = true;
 
-                if (config.debug) {
-                    $notification.success("ToggleSwitch loaded", "ABAdmin Directives", config.notificationDelay);
-                }
-            }]);
+      if (config.debug) {
+        $notification.success(
+          "ToggleSwitch loaded",
+          "ABAdmin Directives",
+          config.notificationDelay
+        );
+      }
+    }
+  ]);
 })();
